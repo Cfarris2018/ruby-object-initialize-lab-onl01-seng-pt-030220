1a4d03635c0a84972f2aa36c4972d81692ad41d1
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-27 21:42:55 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+#1. Define a class with a custom initialize routine.
+#2. Set instance variable attributes from initialize.
+#3. Include a default argument for an initialize 
+
+class Dog 
+  def 
+
+
+end
